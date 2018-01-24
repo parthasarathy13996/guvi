8b7c61505eef804a9d4ec0089b5de4d7e393965e
parthasarathy13996 <35767842+parthasarathy13996@users.noreply.github.com>
2018-01-24 18:13:49 +0530

    Create B_S11.java

---
+++ B_S11.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class B_S11
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+    if(n<100000)
+        {
+    if(n<0)
+    {
+    System.out.println("NEGATIVE NUMBER");
+    }
+    else if(n>0)
+    {
+    System.out.println("POSITIVE NUMBER");
+    }
+    else
+    {
+    System.out.println("ZERo");
+    }}
+}
+}
