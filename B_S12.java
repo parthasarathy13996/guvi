0988559797b0a3fed2fbc8dd190f6028a0ab5421
parthasarathy13996 <35767842+parthasarathy13996@users.noreply.github.com>
2018-01-24 18:20:04 +0530

    Create B_S12.java

---
+++ B_S12.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class B_S12
+{
+    public static void main(String args[])
+    {
+        Scanner sr=new Scanner(System.in);
+        int n=sr.nextInt();
+    
+    if(n<100000 && n>0)
+    {
+        if(n%2==0){
+        System.out.println("Even Number");
+        }
+        else{
+        System.out.println("ODD Number");
+        }
+    }
+}
+}
